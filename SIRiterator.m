af0b09f6eb39e7bde0b2692717a7d7cfd277126a
ga-cha <gabriella.chan1@outlook.com>
2023-07-28 20:24:17 +1000

    update for variable number of both risk and clearance genes

--- SIRiterator.m
+++ SIRiterator.m
@@ -15,26 +15,29 @@
 % From this we derive theortical atrophy per region for each gene and
 % correlate with empirical atrophy per region per gene.
 
-function [gene_corrs, sim_atrophy] = SIRiterator(N_regions, v, dt, T_total, GBA, genes, ...
+function [gene_corrs, sim_atrophy] = SIRiterator(N_regions, v, dt, T_total, clear_genes, risk_genes, ...
     sconnLen, sconnDen, ROIsize, seed, syn_control, init_number, prob_stay, ...
     trans_rate, emp_atrophy)
 
-gene_corrs = table('Size', [width(genes), 3], ...
-    'VariableTypes', {'string', 'double', 'double'}, ...
-    'VariableNames', {'gene', 'correlation', 't'});
+gene_corrs = table('Size', [width(risk_genes)*width(clear_genes), 4], ...
+    'VariableTypes', {'string', 'string', 'double', 'double'}, ...
+    'VariableNames', {'risk gene', 'clearance gene', 'correlation', 't'});
 
-for gene = 1:width(genes)
-    tic
-    gene_name = genes.Properties.VariableNames{gene};
+for risk_gene = 1:width(risk_genes)
+    %tic
+    gene_name = risk_genes.Properties.VariableNames{risk_gene};
     disp(gene_name)
-    %%%%% simulation ------ >>>
-    [Rnor_all, Rmis_all] = SIRsimulator(N_regions, v, dt, T_total, GBA, ...
-        genes(:,gene), sconnLen, sconnDen, ROIsize, seed, syn_control, ...
-        init_number, prob_stay, trans_rate);
-    [sim_atrophy] = SIRatrophy(Rnor_all, Rmis_all, sconnDen, N_regions, dt);
-    [corr, tstep] = SIRcorr(sim_atrophy, emp_atrophy);
-    gene_corrs(gene,:) = {gene_name, corr, tstep};
-    toc
+    for clear_gene = 1:width(clear_genes)
+        %%%%% simulation ------ >>>
+        clear_name = clear_genes.Properties.VariableNames{clear_gene};
+        [Rnor_all, Rmis_all] = SIRsimulator(N_regions, v, dt, T_total, clear_genes(:,clear_gene), ...
+            risk_genes(:,risk_gene), sconnLen, sconnDen, ROIsize, seed, syn_control, ...
+            init_number, prob_stay, trans_rate);
+        [sim_atrophy] = SIRatrophy(Rnor_all, Rmis_all, sconnDen, N_regions, dt);
+        [corr, tstep] = SIRcorr(sim_atrophy, emp_atrophy);
+        gene_corrs(risk_gene,:) = {gene_name, clear_name, corr, tstep};
+    end
+    %toc
 end
 
 gene_corrs = sortrows(gene_corrs, 2);
