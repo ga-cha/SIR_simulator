ab436a4192a25b7350bc750751e483701aae1e60
ga-cha <gabriella.chan1@outlook.com>
2023-07-28 19:30:44 +1000

    generated some preliminary data for UPS and lysosomal clearance

--- SIRcorr.m
+++ SIRcorr.m
@@ -10,12 +10,20 @@
 % each timepoint, then, we can calculate empirical correlation. We then
 % return the maximum.
 
-function [corr] = SIRcorr(sim_atrophy, emp_atrophy)
+function [corr, tstep] = SIRcorr(sim_atrophy, emp_atrophy)
     % something like: for each timepoint, pair atrophy values. 
     %for roi = 1:length(sim_atrophy)
     % Calculate the correlation coefficient
-    corr_mat = corrcoef(sim_atrophy(:, 20000), emp_atrophy);
+    max_corr = zeros(2, 2);
+    tstep = 0;
+    for i = 1:20000
+        corr_mat = corrcoef(sim_atrophy(:, i), emp_atrophy);
+        if max_corr(1, 2) < corr_mat (1, 2)
+            max_corr = corr_mat;
+            tstep = i;
+        end
+    end
     % extract the correlation coefficient
-    corr = corr_mat(1, 2);
+    corr = max_corr(1, 2);
     %end
 end
