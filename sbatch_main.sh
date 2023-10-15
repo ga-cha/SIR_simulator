a718046ad560e2e44e18789a49f414fbf6c9173b
Gabriella Chan (Monash University) <gchan@m3p007.massive.org.au>
2023-10-15 19:17:06 +1100

    run SIR simulation with GO annotated genes. BGS empirical atrophy > 0.

--- sbatch_main.sh
+++ sbatch_main.sh
@@ -18,7 +18,7 @@ while IFS=',' read -r clear_gene; do
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=20:00:00
+#SBATCH --time=24:00:00
 #SBATCH --job-name=sir-simulator
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=gabriella.chan@monash.edu
@@ -27,6 +27,7 @@ while IFS=',' read -r clear_gene; do
 module load matlab/r2023a
 
 # Run main with each clearance gene
+# clear_gene_tr="$(echo -e "${clear_gene}" | tr -d '[:space:]')"
 matlab -nodisplay -r "main('$clear_gene'); exit;"
 EOL
 done < "$input_csv"
